677efba5b6322cf21f22546ae34fbb77fc0456dc
Sampath <kavilashtech@gmail.com>
2022-12-09 18:51:19 +0530

    updated scroller

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <div class="col-xs-12">
 	<marquee behavior="scroll" direction="left" onmouseover="this.stop();" onmouseout="this.start();">
 		<span style="color:red;"><img src="img/new1.gif" height="25px" width="35px">
-			<strong>&nbsp;&nbsp;&nbsp;Abstract Submission Opens on 1st January, 2024. </strong> &nbsp;&nbsp;&nbsp;
+			<strong>&nbsp;&nbsp;&nbsp;Abstract Submission Opens on 1st January, 2023. </strong> &nbsp;&nbsp;&nbsp;
 		</span>
 
 	</marquee>
