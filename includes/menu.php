9d78af2c2f74706aa7d00a5cea041039b9c7f92e
Sampath <kavilashtech@gmail.com>
2022-12-09 18:47:57 +0530

    adding first version

---
+++ includes/menu.php
@@ -0,0 +1,22 @@
+<!-- Start new navigation -->
+
+<!-- <nav class="navbar"> -->
+<nav class="sticky-top">
+	<div class="navbar navbar-expand-md mx-auto sticky-top">
+		<i class='bx bx-menu'></i>
+		<div class="nav-links mr-auto">
+			<ul class="links navbar-left">
+				<li>
+					<a href="index.php" class="active">Home&nbsp;</a>
+				</li>
+				<li>
+					<a href="docs/icons_first_circular.pdf" target="_blank">First Circular</a>
+				</li>
+			</ul>
+		</div>
+
+	</div>
+</nav>
+
+
+<!-- End new navigation-->
---
+++ index.php
@@ -0,0 +1,34 @@
+<?php include 'includes/header.php'; ?>
+
+<!--- Marquee section Starts -->
+<section class="home-marquee">
+<div class="col-xs-12">
+	<marquee behavior="scroll" direction="left" onmouseover="this.stop();" onmouseout="this.start();">
+		<span style="color:red;"><img src="img/new1.gif" height="25px" width="35px">
+			<strong>&nbsp;&nbsp;&nbsp;Abstract Submission Opens on 1st January, 2024. </strong> &nbsp;&nbsp;&nbsp;
+		</span>
+
+	</marquee>
+</div>
+</section>
+<!--- Marquee section Ends -->
+
+<div class="container">
+	<div class="row">
+		<div class="col-12">
+			<img src="img/carousel/img2.jpg" alt="">
+		</div>
+	</div>
+</div>
+
+
+<!-- Main Page Heading -->
+
+
+
+
+
+
+
+
+
